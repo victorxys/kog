42c940c6ef53b367cb7c6dae2fde87dba2a69002
victorxys <victor.xys@gmail.com>
2025-09-21 00:53:35 +0800

    根据您的要求和 Conventional Commits 规范，我为您草拟了以下提交信息：
        1 feat(ui): 重构统计页面并修复多项UI/UX错误
        2
        3 本次提交对 `stat.php`, `kog_summary.php`, 和 `player.php`
          进行了全面的UI现代化重构，并修复了一系列在桌面端和移动端的交互及显示问题。
        4
        5 主要变更:
        6 - **重构 (Refactor)**:
        7     - `stat.php`, `kog_summary.php`, `player.php`
          现已完全采用Aragon主题框架，统一了页眉、页脚和整体布局，移除了过时的内联样式和混乱的
          HTML结构。
        8
        9 - **功能 (Feature)**:
       10     - 在 `stat.php` 的 "Killer" 视图中，恢复了之前缺失的 "All In 胜率", "推 All In
          输了", 和 "接 All In 赢了" 三个统计表格。
       11
       12 - **修复 (Fix)**:
       13     - **桌面端**: 修正了 `kog_header.php`
          中侧边栏的HTML结构，恢复了“x”按钮的折叠功能，解决了主内容区域无法自适应扩宽的问题。
       14     - **移动端**:
       15         - 解决了 `stat.php` 顶部标签页在小屏幕上换行的问题，改为横向滑动。
       16         - 修复了 `player.php` 页面因未加载JS而无法弹出侧边栏的问题。
       17     - **样式**: 修正了 `kog_summary.php` 中 "Rebuy" 事件的徽章颜色，使其清晰可见。
       18     - **控制台警告**: 为Highcharts图表添加了 `accessibility.js`
          模块，消除了相关的浏览器警告。

--- kog/kog_lucky.php
+++ kog/kog_lucky.php
@@ -1,220 +1,125 @@
 <?php
-require_once('functions.php'); 
+require_once('functions.php');
 
-require_once ("kog_header.php");
+// Display logic first
+require_once("kog_header.php");
 
 if (empty($_REQUEST['uid'])) {
-	die("缺少UID");
+    die("缺少UID");
 }
 
-$game 			= 	get_game($_REQUEST['gid']);
-$game_player 	= 	get_game_player($_REQUEST['gid']);
+$game = get_game($_REQUEST['gid']);
+$game_player = get_game_player($_REQUEST['gid']);
 
+$game_player_select_array = [];
 foreach ($game_player as $key => $value) {
-	// if ($value->uid == $_REQUEST['uid']) {
-	// 	continue;
-	// }
-	$game_player_select[$key] = $value;
-	$game_player_select_array[$value->uid] = $value->player;
+    $game_player_select_array[$value->uid] = $value->player;
 }
-if (isset($game_player_select_array)) {
-	$game_player_json = serialize($game_player_select_array);
-}
-
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Rebuy!</title>
-<!-- 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="style.css"> -->
-	<style type="text/css">
-	a{
-		color: #000;
-	}
-	.cell1-3{
-		float: left;
-		width: 33%;
-		text-align: center;
-	}
-	.row-full{
-		float: left;
-		width: 100%;
-		text-align: center;
-		padding-bottom: 5px;
-	}
-	.row-full-heigh{
-		float: left;
-		width: 100%;
-		text-align: center;
-		padding-bottom: 5px;
-		line-height: 50px;
-	}
-	.row-full-boder-bottom{
-		float: left;
-		width: 100%;
-		text-align: center;
-		padding-bottom: 5px;
-		border-top-style: solid;
-		border-color: #8a8a8a;
-		border-width: 1px
-		padding-right:5px;
-		padding-left: 5px;
-	}
-	.cell1-5{
-		float: left;
-		width: 20%;
-		line-height: 82px;
-	}
-	.cell3-5{
-		float: left;
-		width: 60%;
-	}
-	.content{
-		padding-top: 20px;
-	}
-	.cell1-5-heigh{
-		float: left;
-		width: 20%;
-		line-height:30px;
-	}
-	.cell1-5-heigh{
-		float: left;
-		width: 20%;
-		line-height:30px;
-	}
-</style>
-</head>
-<body>
-	<form method="get">
-	<div class="container-fluid mt--6">
-      	<div class="row justify-content-center">
-	        <div class="col-lg-12 card-wrapper ct-example">
-	        	<div class="card">
-	            <div class="card-header">
-	              	<h3 class="mb-0">Lucky One: <?php _e( get_player($_REQUEST['uid'])[0]->nickname)?></h3>
-	            </div>
-	            <div class="card-body">
 
+<div class="container-fluid py-4">
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="mb-0">Lucky One: <?php _e(get_player($_REQUEST['uid'])[0]->nickname) ?></h3>
+                </div>
+                <div class="card-body">
+                    <form method="post">
+                        <div class="form-group row">
+                            <label class="col-md-2 col-form-label form-control-label">Dealer</label>
+                            <div class="col-md-10">
+                                <?php erp_html_form_input(array(
+                                    'name' => 'dealar',
+                                    'class' => 'form-control',
+                                    'type' => 'select',
+                                    'required' => true,
+                                    'options' => array('' => '- Select -') + $game_player_select_array
+                                ));
+                                ?>
+                            </div>
+                        </div>
 
-		<div class="col-lg-12 card-wrapper ct-example">
-			<div class="form-group row" style="margin-bottom: 1rem">
-				<label for="example-password-input" class="col-md-2 col-form-label form-control-label">Dealer</label>
-				
-				<div class="col-md-10">
+                        <div class="form-group row">
+                            <label class="col-md-2 col-form-label form-control-label">Lucky Type</label>
+                            <div class="col-md-10">
+                                <?php erp_html_form_input(array(
+                                    'name' => 'lucky_type',
+                                    'class' => 'form-control',
+                                    'type' => 'select',
+                                    'required' => true,
+                                    'options' => array('' => '- Select -') + get_lucky_bonus()
+                                ));
+                                ?>
+                            </div>
+                        </div>
 
-					<?php erp_html_form_input( array(
-                                'name'    => 'dealar',
-                                'class'   => 'form-control',
-                                'type'    => 'select',
-                                'id'      => 'dealar',
-                        		'required' => true,
-                                'options' => array( '' => '- Select -' ) + $game_player_select_array
-                            ) ); 
-					?>
-				</div>
-			</div>
+                        <div class="form-group row">
+                            <label class="col-md-2 col-form-label form-control-label">Cards</label>
+                            <div class="col-md-10">
+                                <?php erp_html_form_input(array(
+                                    'name' => 'cards',
+                                    'class' => 'form-control',
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => '',
+                                ));
+                                ?>
+                            </div>
+                        </div>
 
-			<div class="form-group row" >
-				<label for="example-password-input" class="col-md-2 col-form-label form-control-label">Lucky Type</label>
-				
-				<div class="col-md-10">
+                        <div class="form-group row">
+                            <label class="col-md-2 col-form-label form-control-label">Bonus</label>
+                            <div class="col-md-10">
+                                <?php erp_html_form_input(array(
+                                    'name' => 'bonus',
+                                    'class' => 'form-control',
+                                    'type' => 'text',
+                                    'required' => true,
+                                    'value' => '',
+                                ));
+                                ?>
+                            </div>
+                        </div>
 
-					<?php erp_html_form_input( array(
-                                'name'    => 'lucky_type',
-                                'class'   => 'form-control',
-                                'type'    => 'select',
-                                'id'      => 'lucky_type',
-                        		'required' => true,
-                                'options' => array( '' => '- Select -' ) + get_lucky_bonus()
-                            ) ); 
-					?>
-				</div>
-			</div>
+                        <div class="text-center">
+                            <input type="hidden" name="action" value="lucky">
+                            <input type="hidden" name="uid" value="<?php _e($_REQUEST['uid']) ?>">
+                            <input type="hidden" name="gid" value="<?php _e($_REQUEST['gid']) ?>">
+                            <button type="submit" class="btn btn-primary btn-lg w-100">Lucky</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-			<div class="form-group row" style="margin-bottom: 1rem">
-				<label for="example-password-input" class="col-md-2 col-form-label form-control-label">Cards</label>
-				
-				<div class="col-md-10">
-					<?php erp_html_form_input( array(
-						'name'    	=> 'cards',
-						'class'   	=> 'form-control',
-						'type'    	=> 'text',
-						'required' 	=> 'true',
-						'value' 	=> '',
-					) ); 
-					?>
-				</div>
-			</div>
-
-			<div class="form-group row" style="margin-bottom: 1rem">
-				<label for="example-password-input" class="col-md-2 col-form-label form-control-label">Bonus</label>
-				
-				<div class="col-md-10">
-					<?php erp_html_form_input( array(
-						'name'    	=> 'bonus',
-						'class'   	=> 'form-control',
-						'type'    	=> 'text',
-						'required' 	=> 'true',
-						'value' 	=> '',
-					) ); 
-					?>
-				</div>
-			</div>
-
-			
-		</div>
-			
-			
-			<div style="text-align: center;padding-top: 10px">
-				<?php erp_html_form_input( array(
-					'name'    	=> 'action',
-							// 'help'    	=> '-',
-							// 'value'   	=> isset($_REQUEST['start_time'])?$_REQUEST['start_time']:'',
-					'type'    	=> 'hidden',
-					'value' 	=> 'lucky',
-				) ); 
-				?>
-				<input type="hidden" name="uid" value="<?php _e($_REQUEST['uid'])?>">
-				<input type="hidden" name="gid" value="<?php _e($_REQUEST['gid'])?>">
-				<input type="hidden" name="player" value='<?php _e($game_player_json)?>'>
-				<!-- <input type="submit" value="Rebuy!" class="btn btn-primary btn-lg btn-block" > -->
-				<button type="submit" class="btn btn-primary btn-lg btn-block">Lucky</button>
-			</div>
-	            </div>
-	        </div>
-    	</div>
-	</div>		
-	</form>
-</body>
 <?php
-if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'lucky') {
-	if(creat_lucky_info($_REQUEST['uid'])){
-		$url = "kog_detail.php?gid=".$_REQUEST['gid'];
-			echo '
-				<script type="text/javascript">
-				document.addEventListener("DOMContentLoaded", function() {
-					Swal.fire({
-						title: "Success",
-						text: "Let\'s all in again!",
-						icon: "success",
-						timer: 2000,
-						timerProgressBar: true,
-						showConfirmButton: false
-					}).then((result) => {
-						/* 定时器结束后或者用户意外关闭弹窗后都会跳转 */
-						window.location.href = ' . json_encode($url) . ';
-					});
-				});
-				</script>
-			';
-	}
+// --- PROCESS FORM SUBMISSION AT THE END ---
+if (isset($_POST['action']) && $_POST['action'] == 'lucky') {
+    if (creat_lucky_info($_POST['uid'])) {
+        $url = "kog_detail.php?gid=" . $_POST['gid'];
+        $swal_script = <<<HTML
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        Swal.fire({
+            title: "Success",
+            text: "Let's all in again!",
+            icon: "success",
+            timer: 1500,
+            timerProgressBar: true,
+            showConfirmButton: false
+        }).then((result) => {
+            window.location.href = '{$url}';
+        });
+    });
+</script>
+HTML;
+        echo $swal_script;
+    }
 }
+
+require_once "kog_footer.php";
 ?>
-
-
-
-</html>
